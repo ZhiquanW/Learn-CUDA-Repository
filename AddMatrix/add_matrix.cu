2ac474a1f83d217cae2494361b6de6c75863c304
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-04-02 19:32:50 +0800

    Practise:Add Matrix

--- AddMatrix/add_matrix.cu
+++ AddMatrix/add_matrix.cu
@@ -1,9 +1,10 @@
 #include <iostream>
-const int N = 16;
+const int N = 1000;
 __global__ void add(float a[N][N], float b[N][N]);
+using namespace std;
 int main() {
-  float A[N][N];
-  float B[N][N];
+  float(*A)[N];
+  float(*B)[N];
 
   // Allocate Unified Memory – accessible from CPU or GPU
   cudaMallocManaged(&A, N * N * sizeof(float));
@@ -15,23 +16,30 @@ int main() {
       B[i][j] = 4.0;
     }
   }
-  dim3 block_dim(N, N);
-  add<<<1, block_dim>>>(A, B);
+  dim3 block_dim(16, 16);
+  dim3 grid_dim((N - 1) / block_dim.x + 1, (N - 1) / block_dim.y + 1);
+  add<<<grid_dim, block_dim>>>(A, B);
   cudaDeviceSynchronize();
 
   float error_sum = 0;
   for (int i = 0; i < N; ++i) {
     for (int j = 0; j < N; ++j) {
-      error_sum += 5 - A[i][j];
+      error_sum += 5.0 - B[i][j];
+      if (B[i][j] != 5) {
+        cout << i << " " << j << " " << B[i][j] << endl;
+      }
     }
   }
   std::cout << error_sum << std::endl;
+
   cudaFree(A);
   cudaFree(B);
 }
 
-__global__ void add(float a[N][N], float b[N][N]) {
-  int x = threadIdx.x;
-  int y = threadIdx.y;
-  b[x][y] = a[x][y] + b[x][y];
+__global__ void add(float a[][N], float b[][N]) {
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  if (x < N && y < N) {
+    b[x][y] = a[x][y] + b[x][y];
+  }
 }
